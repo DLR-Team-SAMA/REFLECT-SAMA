494d726aa632c89831aae604fc9f6a52356c1563
“Abivishaq” <abivishaq@gatech.edu>
2024-04-19 23:56:49 -0400

    pipeline VLMSGG testing

--- YOLO-open_world backbone code.py
+++ YOLO-open_world backbone code.py
@@ -1,18 +1,17 @@
 from ultralytics import YOLO
 import supervision as sv
-import cv2 
+# import cv2 
 
 bounding_box_annotator = sv.BoundingBoxAnnotator()
 label_annotator = sv.LabelAnnotator()
-img = 'frame_52.jpg'
+img = 'test_images/frame_52.jpg'
 model = YOLO('yolov8s-world.pt')
-model.set_classes(['mug'])
-results = model.predict('frame_52.jpg')
+model.set_classes(['mug','coffee_machine'])
+results = model.predict(img)
 
 results[0].show()
 
 detections = sv.Detections.from_ultralytics(results[0])
-annotated_frame = bounding_box_annotator.annotate(scene = img.copy(), detections=detections)
-annotated_frame = label_annotator.annotate(scene = annotated_frame, detections = detections)
+# annotated_frame = bounding_box_annotator.annotate(scene = img.copy(), detections=detections)
+# annotated_frame = label_annotator.annotate(scene = annotated_frame, detections = detections)
 
-print(detections)
--- pipeline.py
+++ pipeline.py
@@ -1,28 +1,29 @@
-from dummy import DummySGG
+from VLM_sgg import VLM_SGG
 from utils import bboxes_to_rois
 import PIL.Image
+from dummy import DummySGG
 
-sgg1 = DummySGG()
-sgg2 = sgg1
+sgg2 = DummySGG()
+sgg1 = VLM_SGG('models/gemini-1.5-pro-latest')
 
 img = PIL.Image.open('test_images/frame_15.jpg')
 # get object list
-object_list = sgg1.object_list_detector('test_images/frame_15.jpg')
+object_list = sgg1.object_list_detector(img)
 print(object_list)
 
 # get object bounding boxes
-object_bboxes, object_labels = sgg2.object_detector(img, object_list,'test_images/frame_15.jpg')
+object_bboxes, object_labels = sgg2.object_detector(img, object_list)
 print(object_labels)
 print(object_bboxes)
 
 rois = bboxes_to_rois(img,object_bboxes)
 
 # get states 
-states = sgg1.state_detector(img, rois, [], 'test_images/frame_15.jpg')
+states = sgg1.state_detector(img, rois, object_labels, [])
 print(states)
 
 # get edges
-edges = sgg1.edge_detector(img, rois, [], 'test_images/frame_15.jpg')
+edges = sgg1.edge_detector(img, rois, object_labels, [])
 print(edges)
 
 # get scene graph
